559a319c262a7350b83c12a2b57970f9e1cfca44
Michael Rapp <michael.rapp90@googlemail.com>
2016-02-19 02:25:42 +0100

    Implemented the functionality of the preference, which allows to show a bottom sheet.

--- example/src/main/java/de/mrapp/android/bottomsheet/example/PreferenceFragment.java
+++ example/src/main/java/de/mrapp/android/bottomsheet/example/PreferenceFragment.java
@@ -15,6 +15,11 @@
 package de.mrapp.android.bottomsheet.example;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+
+import de.mrapp.android.bottomsheet.BottomSheet;
+import de.mrapp.android.bottomsheet.BottomSheet.Style;
 
 /**
  * A preference fragment, which contains the example app's settings.
@@ -23,10 +28,50 @@ import android.os.Bundle;
  */
 public class PreferenceFragment extends android.preference.PreferenceFragment {
 
+    /**
+     * Initializes the preference, which allows to show a bottom sheet.
+     */
+    private void initializeShowBottomSheetPreference() {
+        Preference showBottomSheetPreference =
+                findPreference(getString(R.string.show_bottom_sheet_preference_key));
+        showBottomSheetPreference
+                .setOnPreferenceClickListener(createShowBottomSheetPreferenceListener());
+    }
+
+    /**
+     * Creates and returns a listener, which allows to show a bottom sheet.
+     *
+     * @return The listener, which has been created, as an instance of the type {@link
+     * OnPreferenceClickListener}
+     */
+    private OnPreferenceClickListener createShowBottomSheetPreferenceListener() {
+        return new OnPreferenceClickListener() {
+
+            @Override
+            public boolean onPreferenceClick(final Preference preference) {
+                BottomSheet.Builder builder = new BottomSheet.Builder(getActivity());
+                builder.setStyle(Style.LIST_COLUMNS);
+                builder.addItem("Item 1");
+                builder.addItem("Item 2");
+                builder.addItem("Item 3");
+                builder.addItem("Item 4");
+                builder.addItem("Item 5");
+                builder.addItem("Item 6");
+                builder.addItem("Item 7");
+                builder.addItem("Item 8");
+                builder.addItem("Item 9");
+                builder.show();
+                return true;
+            }
+
+        };
+    }
+
     @Override
     public final void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        initializeShowBottomSheetPreference();
     }
 
 }
